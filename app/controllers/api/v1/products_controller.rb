29169abb234db515d5433d9092b4fbe7f9f9fe83
Yancy Zhang <yuanxi.zhang@gmail.com>
2021-05-11 13:50:59 -0700

    Add products controller, update route.rb and seed.rb

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  # GET /products
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  # GET /todos/:id
+  def show
+    render json: @product
+  end
+
+   # POST /products
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /products/:id
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/:id
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :brand, :description, :instock)
+  end
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def create_product(name, brand, description, instock)
+    Product.create([{
+        name: name, 
+        brand: brand,
+        description: description,
+        instock: instock
+    }])
+end
+
+
+create_product("Banana", "Chiquita", "Organic", 30)
+create_product("Apple Juice", "Martinelli", "Organic, 10 oz. glass jar", 20)
+create_product("Vitamin C", "Nature's Way", "500 Mg, 250 capsules", 10)
