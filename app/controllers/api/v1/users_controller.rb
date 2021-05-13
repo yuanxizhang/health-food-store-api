ab4a3927364c191cdff9dac48c04f2ab0e577101
Yancy Zhang <yuanxi.zhang@gmail.com>
2021-05-12 19:51:25 -0700

    Update users_controller and user_serializer with name field

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,6 +18,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :isadmin)
+    params.require(:user).permit(:email, :password, :isadmin, :name)
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :isadmin
+  attributes :id, :email, :isadmin, :name
 end
